5c3ad94b847e1f473cb13fed9d49e8364349ed58
nadiajali <nadiajali@hotmail.com>
2020-10-26 13:25:11 -0400

    Second Commit

---
+++ build/precache-manifest.b1273f8fd6eaaa7174564bc6359b33a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812293be22dd1c456b486b993760335c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f5b92ec1b6f80c3f6d0",
+    "url": "/static/css/main.bb93fdff.chunk.css"
+  },
+  {
+    "revision": "6e9c4210f9b8a88c10cb",
+    "url": "/static/js/2.413cc550.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.413cc550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f5b92ec1b6f80c3f6d0",
+    "url": "/static/js/main.7af78f31.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "f662a7562ad4327f11559d4c5a46ad8e",
+    "url": "/static/media/house.f662a756.jpg"
+  }
+]);
