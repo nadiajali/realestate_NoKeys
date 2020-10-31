b108e94a9e094ce0aa63b6a7b181df37288735c6
nadiajali <nadiajali@hotmail.com>
2020-10-31 10:57:31 -0400

    Updated for S3

--- realestate/settings.py
+++ realestate/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'realtors',
     'listings',
     'contacts'
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -145,8 +146,15 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = 'nadiajali-realestate'
+AWS_S3_REGION_NAME = 'us-east-2'
+
+AWS_ACCESS_KEY_ID = 'AKIA4TAYIJEHVVQGW3OS'
+AWS_SECRET_ACCESS_KEY = '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'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
