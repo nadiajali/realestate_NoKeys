3b051dac3d2968212f62a3b2beba5417b86e44de
nadiajali <nadiajali@hotmail.com>
2020-11-24 09:19:31 -0500

    Updated Settings File

--- realestate/settings.py
+++ realestate/settings.py
@@ -149,13 +149,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# AWS_STORAGE_BUCKET_NAME = 'nadiajali-realestate'
-# AWS_S3_REGION_NAME = 'us-east-2'
-
-# AWS_ACCESS_KEY_ID = ''
-# AWS_SECRET_ACCESS_KEY = ''
-
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication'],
